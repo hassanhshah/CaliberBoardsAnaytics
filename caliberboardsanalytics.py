e6af6147ebf8535c62dbfb7e082e96ae42e3ef32
HHS <98497536+hassanhshah@users.noreply.github.com>
2023-07-31 14:22:09 +0000

    Update caliberboardsanalytics.py

--- caliberboardsanalytics.py
+++ caliberboardsanalytics.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
 from emoji import emojize
+from scipy import stats
 
 title = ("Caliber Boards Analytics " + emojize(":hospital:"))
 st.title(title)
@@ -32,6 +35,47 @@ config = {"displayModeBar": False, "showTips": False}
 
 # Display the figure in Streamlit with specified config
 st.plotly_chart(fig, config=config)
+
+data = pd.read_csv('/Users/hassanhshah/Downloads/data-1689557843429.csv')
+questions = pd.read_csv('/Users/hassanhshah/Downloads/AverageQuestion.csv')
+
+data['prompt'] = data['prompt'].fillna('')
+data['question'] = data['question'].fillna('')
+
+questions['prompt'] = questions['prompt'].fillna('')
+questions['question'] = questions['question'].fillna('')
+
+data['prompt_and_question'] = data['prompt'] + " / " + data['question']
+questions['prompt_and_question'] = questions['prompt'] + " / " + questions['question']
+
+selected_scenario = st.selectbox('Select a Scenario:', sorted(data['scenario_name'].unique()))
+
+grouped_data = data.groupby(['scenario_name', 'candidate_id'])['candidate_score_per_question'].sum().reset_index()
+
+# Analyze entire scenario
+filtered_data = grouped_data[grouped_data['scenario_name'] == selected_scenario]
+total_correct_answers = questions.loc[questions['scenario_name'] == selected_scenario, 'total_correct_answers'].sum()
+
+# Input for number of correct answers
+correct_answers = st.number_input('Enter the number of correct answers:', min_value=0, step=1)
+
+# Calculate average score for the selected scenario
+average_score = filtered_data['candidate_score_per_question'].mean() if not filtered_data.empty else 0
+
+# Calculate percentile rank of the user's score
+percentile = stats.percentileofscore(filtered_data['candidate_score_per_question'], correct_answers) if not filtered_data.empty else 0
+
+# Display the results
+with st.container():
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.metric(label="Total Score", value=total_correct_answers, delta=None)
+    with col2:
+        st.metric(label="Your Score", value=correct_answers, delta=None)
+    with col3:
+        st.metric(label="Average Score", value=f"{average_score:.2f}", delta=None)
+    with col4:
+        st.metric(label="Your Percentile", value=f"{percentile:.2f}", delta=None)
 
 st.subheader('Optimize Success: Higher Pass Rates with More Sessions')
 st.markdown('**Data based on 2022 ABS Certifying Examination statistics available [here](https://absurgerydata.shinyapps.io/PassRates/)**')
